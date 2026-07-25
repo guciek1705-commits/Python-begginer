f6ee804d55d5f54aa5fca2ea138eb13377cda225
IgorDev <guciek1705@gmail.com>
2026-07-25 23:41:17 +0200

    Notes.main/dictionares.all

---
+++ Notes.main/Variables.start.py
@@ -0,0 +1,44 @@
+# Variable = A container for a value (string, integer, float, boolean)  
+#            A variable behaves as if it was the value it contains  
+  
+  
+# Strings  TEXT
+first_name = "Igor"  
+food = "taco"  
+email = "gucik1705@gmail.com"  
+  
+
+# Integers  NUMBERS
+age = 25  
+quantity = 13  
+num_of_students = 44  
+  
+
+# Float  for example 3.14, 9.99, 0.5 prices/distance just not rounded numbers
+price = 10.99  
+gpa = 3.1  
+distance = 22.5  
+  
+  
+print(f"The price is ${price}")  
+print(f"My gpa is: {gpa}")  
+print(f"Half maraton is {distance}km")
+
+
+
+
+# Boolean = True or False value
+is_student = True  
+for_sale = False  
+if is_online:    
+  
+    print("That item is for sale")  
+else:  
+    print("That item is NOT avaliable")
+      
+if is_online:  
+    print("You are online")  
+else:  
+    print("You are offline")
+
+
